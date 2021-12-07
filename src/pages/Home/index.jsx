98afccfa42ab8a44c7dd38253673dbd7d2c457af
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-07 16:05:04 +0100

    add themeProvider and  setup theme basic rules + palette

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-       
+import { Button } from '@mui/material' 
+
 const Home = () => {
 
   return (
     <div className=''>
+      <Button>Salut</Button>      
     </div>
   )
 }
