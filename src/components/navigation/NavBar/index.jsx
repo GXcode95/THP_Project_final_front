5d9bde7a773154de4e1dbcc39c2b0069614f30a3
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-07 22:21:36 +0100

    create CartButton and RentButton + add it to the navbar

--- src/components/navigation/NavBar/index.jsx
+++ src/components/navigation/NavBar/index.jsx
@@ -6,27 +6,31 @@ import Box from '@mui/material/Box';
 import LoginButton from 'components/buttons/LoginButton';
 import {Toolbar} from '@mui/material'
 import logo from 'assets/images/logo.png'
+import CartButton from 'components/buttons/CartButton'
+import RentButton from 'components/buttons/RentButton'
+
 
 const NavBar = ()  => {
   // const user = useSelector(state => state.userReducer.user)
-
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar >
-          <Box 
-            sx={{
-              display: "flex",
-              justifyContent: "space-between", 
-              alignItems: "center",  
-              width: "100%"
-            }}
-          >
-            <img src={logo} alt="playbox logo" height="50px"/> 
+        <Toolbar  
+          sx={{
+            display: "flex",
+            justifyContent: "space-between", 
+            alignItems: "center",  
+            width: "100%"
+          }} 
+        >
+          <img src={logo} alt="playbox logo" height="50px"/> 
+          <Box>
             {/* {
               user && user.id ?
               <AvatarDropdown /> : <LoginButton /> 
             } */}
+            <CartButton color="white"/>
+            <RentButton color="white"/>
             <LoginButton />
           </Box>
         </Toolbar>
