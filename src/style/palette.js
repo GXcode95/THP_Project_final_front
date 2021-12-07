5f77414668ce8845f40fb3d681426d7c16cd4dd4
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-07 21:10:21 +0100

    change palette

--- src/style/palette.js
+++ src/style/palette.js
@@ -3,8 +3,44 @@ import {createTheme} from '@mui/material'
 export const light = createTheme({
   palette: {
     mode: 'light',
-  }
+    primary: {
+      main: '#283845', // original from logo : #11bed8
+    },
+    secondary: {
+      main: '#109eb3',
+    },
+    ternary: {
+      main: '#ffb735',
+    },
+    error: {
+      main: '#ff3a3a',
+    },
+    info: {
+      main: '#f576a0',
+    },
+    warning: {
+      main: '#8622c3',
+    },
+    success: {
+      main: '#43a047',
+    },
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none",
+        },
+      },
+      defaultProps: {
+        variant: "contained",
+        color: "primary",
+        disableRipple: true
+      }
+    } 
+  },
 });
+
 export const dark = createTheme({
   palette: {
     mode: 'dark',
