c6bd6f4bf1fb768614a3b4b3514f18f61154c451
CommitID <hanxcity@gmail.com>
2021-12-08 22:28:34 +0100

    create component HeroBanner

---
+++ src/components/navigation/NavBar/HeroBanner.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import {Card, Box, Typography} from '@mui/material'
+import { Button } from '@mui/material'
+
+const HeroBanner = () => {
+
+  return (
+    <Card sx={{mb:"3em", borderRadius:"0"}} className="HeroBanner">
+      <Box 
+        width="100vw" 
+        height="350px" 
+        display= "flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+        sx={{
+          background:"url(https://i.ibb.co/0YKj2d7/wepik-2021118-22224.jpg) no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          }}
+      >
+        <Typography 
+          variant="h2" 
+          sx={{
+            fontSize: "1.5rem", 
+            letterSpacing: "0.3rem",
+            textShadow: 2
+          }}
+          align="center"
+          color="white.main"
+          fontWeight="600"
+          className="subtitle-hero"
+          >
+          Parce qu'il n'y a pas que des échecs dans la vie
+          
+        </Typography>
+        <Typography 
+          variant="h2" 
+          sx={{
+            fontSize: "4rem", 
+          }}
+          align="center"
+          color="secondary.main"
+          fontWeight="600"
+          className="title-hero"
+          >
+          Louez, testez, achetez !
+        </Typography>
+        <Button
+           sx={{marginTop: "2rem", fontSize: "1.5rem"}}
+           color="secondary"
+        >
+          S'abonner
+        </Button>
+      </Box>
+    </Card>
+  )
+}
+    
+export default HeroBanner
