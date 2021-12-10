3ae510e876d6f202a2233fc08e6941debf5b23a5
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-10 14:33:37 +0100

    remove cart Item

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import CartItem from 'components/CartItem'
 
        
 const Cart = () => {
 
   return (
     <div className=''>
-      <CartItem />
     </div>
   )
 }
