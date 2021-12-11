cd4884714f32db8faad0b9b56ef72ef5f729f5ab
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-11 11:23:47 +0100

    create searchBar component

---
+++ src/components/SearchBar/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Box, Button, TextField} from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
+const SearchBar = () => {
+
+  return (
+    <Box>
+      <TextField>
+
+      </TextField>
+      <Button> OK </Button>
+    </Box>
+  )
+}
+    
+export default SearchBar
