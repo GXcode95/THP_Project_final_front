0e9917f69a52950b1bd5787ce5f90f5896c1d222
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-11 18:50:41 +0100

    add small logo version for mobile in navbar

--- src/components/navigation/NavBar/index.jsx
+++ src/components/navigation/NavBar/index.jsx
@@ -4,10 +4,14 @@ import { useSelector } from 'react-redux';
 import AvatarDropdown from './AvatarDropdown'
 import LoginButton from 'components/buttons/LoginButton';
 import logo from 'assets/images/logo-playbox.svg'
+import logoSmall from 'assets/images/logoSmall.svg'
 import CartButton from 'components/buttons/CartButton'
 import RentButton from 'components/buttons/RentButton'
 import isSigned from 'helpers/isSigned'
 import { Link } from 'react-router-dom'
+import { MobileView, BrowserView } from 'react-device-detect';
+
+
 const NavBar = ()  => {
   const user = useSelector(state => state.userReducer)
   return (
@@ -23,10 +27,15 @@ const NavBar = ()  => {
         >
           <Box display="flex" alignItems="center" gap={2}>
             <Link to='/'>
-              <img src={logo} alt="playbox logo" height="50px"/>
+              <MobileView>
+                <img src={logoSmall} alt="playbox logo" height="50px"/>
+              </MobileView>
+              <BrowserView>         
+                <img src={logo} alt="playbox logo" height="50px"/>
+              </BrowserView>
             </Link>
             <Link to='/jeux'>
-              <Typography variant="h6" component="span">Voir les jeux</Typography>
+              <Typography variant="h6" component="span">Nos jeux</Typography>
             </Link>
           </Box>
           <Box>
