1c35e08e4880afc8145d62e4472296ea11cbd5ea
antcrn <anthonycornu.pro@gmail.com>
2021-12-12 19:30:38 +0100

    fix button in button & redesign stripe button

--- src/components/Pricing/PricingCard.jsx
+++ src/components/Pricing/PricingCard.jsx
@@ -1,26 +1,26 @@
 import React from 'react'
-import {Card, Button,Box, Typography, CardContent, CardActions, CardHeader } from '@mui/material'
+import { Card, Button, Box, Typography, CardContent, CardActions, CardHeader } from '@mui/material'
 import StripeButton from 'components/buttons/StripeButton'
-const PricingCard = ({tier, variant, description}) => {
+const PricingCard = ({ tier, variant, description }) => {
 
   const priceInEuro = (priceInCent) => {
     return priceInCent / 100
   }
 
-  return(
-    <Card elevation={4} sx={{borderRadius: "2px"}}>
+  return (
+    <Card elevation={4} sx={{ borderRadius: "2px" }}>
       <CardHeader
         title={tier.name}
         titleTypographyProps={{ align: 'center' }}
         subheaderTypographyProps={{ align: 'center' }}
-        sx={{ backgroundColor: 'secondary.main', color: 'white.main'}}
+        sx={{ backgroundColor: 'secondary.main', color: 'white.main' }}
       />
-      <CardContent sx={{py:5}}>
+      <CardContent sx={{ py: 5 }}>
         <Box
-            display='flex'
-            justifyContent='center'
-            alignItems='baseline'
-            sx={{ mb: 2}}
+          display='flex'
+          justifyContent='center'
+          alignItems='baseline'
+          sx={{ mb: 2 }}
         >
           <Typography component="h2" variant="h3" color="text.primary">
             {priceInEuro(tier.price)}€
@@ -30,18 +30,16 @@ const PricingCard = ({tier, variant, description}) => {
           </Typography>
         </Box>
         <ul>
-            <Typography
-              component="li"
-              variant="subtitle1"
-              align="center"
-            >
-              {description}
-            </Typography>
+          <Typography
+            component="li"
+            variant="subtitle1"
+            align="center"
+          >
+            {description}
+          </Typography>
         </ul>
       </CardContent>
-      <CardActions>
-          <StripeButton item={tier} quantity={1} variant={variant}/>
-      </CardActions>
+      <StripeButton item={tier} quantity={1} variant={variant} />
     </Card>
   )
 }
--- src/components/buttons/StripeButton/index.jsx
+++ src/components/buttons/StripeButton/index.jsx
@@ -1,32 +1,28 @@
 import React from 'react'
 import StripeCheckout from 'react-stripe-checkout'
 import APIManager from 'services/Api'
-import {Button} from '@mui/material'
-import {useNavigate} from 'react-router-dom'
+import { Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
+import Box from '@mui/material/Box';
 
-const StripeButton = ({item, quantity, variant}) => {
+
+const StripeButton = ({ item, quantity, variant }) => {
   const navigate = useNavigate()
 
   const handleToken = async (token) => {
     const response = await APIManager.buyPackage(token, item.id, quantity)
-    if(!response.error)
+    if (!response.error)
       navigate(0)
   }
 
   return (
-    <Button 
-      fullWidth
-      color="secondary"
-      variant={variant}
-    >
-      J'en profite
-      <StripeCheckout 
-        stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
-        token={handleToken}
-        className='stripe'
-      />
-    </Button>
+    <StripeCheckout
+      stripeKey={process.env.REACT_APP_STRIPE_PUBLIC_KEY}
+      token={handleToken}
+      className='stripe'
+      label="J'en profite"
+    />
   )
 }
-    
+
 export default StripeButton
