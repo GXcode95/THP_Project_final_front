a80a675f49a485be2d3d0cc6b1b2cde237a34005
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-14 22:05:24 +0100

    create a UserAvatar component and add it to Comment

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -17,9 +17,9 @@ const GameCard = ({ game, edit }) => {
 
   const handleCardHeight = () => {
     const screen = window.screen.width
-    if (screen > 1500) {
+    if (screen >= 1500) {
        return 350
-    } else if(screen > 1900) {
+    } else if(screen >= 1900) {
       return 200
     } else {
       return 300
