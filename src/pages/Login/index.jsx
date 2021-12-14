f7066aa4642fcb3343fa117727c9cd14ae9fa005
CommitID <hanxcity@gmail.com>
2021-12-14 12:11:25 +0100

    redesign login page

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import SignIn from 'components/forms/SignIn'
 import SignUp from 'components/forms/SignUp'
 import PasswordReset from 'components/forms/PasswordReset'
-import {Button, Box,Typography} from '@mui/material'
+import signIn from 'assets/images/bg_signin.jpg'
+
 const Login = () => {
   const [isRegistered, setIsRegistered] = React.useState(true)
   const [passwordReset, setPasswordReset] = React.useState(false)
@@ -14,32 +21,81 @@ const Login = () => {
     setPasswordReset(!passwordReset)
   }
 
-  return (
-    <Box 
-      display="flex" 
-      flexDirection="column" 
-      justifyContent="space-evenly"
-      minHeight="90vh"
-    >
-      <Typography variant="h2" color="primary" align="center" >
-        {isRegistered ? "Se connecter" : "S'inscrire"}
-      </Typography>
+  
 
-      {isRegistered ? <SignIn /> : <SignUp /> }
-      
-      <Box display="flex" flexDirection="column">
-        
-        <Button variant="text" onClick={toggleIsRegistered}>
-          {isRegistered ? "S'inscrire" : "Deja inscrit ?"} 
-        </Button>
-        
-        <Button variant="text" onClick={togglePasswordReset}>
-            Mot de passe oublié ?
-        </Button>
-      </Box>
-      {passwordReset && <PasswordReset togglePasswordReset={togglePasswordReset}/>}
-    </Box>
-  )
+
+
+  return (
+    <div>
+      <Grid container component="main" sx={{ height: '30vh' }}>
+        <CssBaseline />
+        <Grid
+          item
+          className="bg-signin"
+          xs={false}
+          sm={4}
+          md={7}
+          sx={{
+            backgroundImage: `url(${signIn})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundColor: (t) =>
+              t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            padding: '5rem'
+          }}
+        />
+        <Grid 
+          item 
+          xs={12} 
+          sm={8} 
+          md={5} 
+          component={Paper} 
+          elevation={10} 
+          square
+        >
+          <Box
+            sx={{
+              my: 8,
+              mx: 4,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            
+            <Typography variant="h2" color="primary.main" align="center" >
+              {isRegistered ? "Se connecter" : "S'inscrire"}
+            </Typography>
+
+            {isRegistered ? <SignIn /> : <SignUp /> }
+
+           
+            <Grid 
+              container
+              justifyContent="center"
+              alignItems="center"
+              paddingLeft="4em"
+            >
+              <Grid item xs>
+                <Button  variant="text" onClick={toggleIsRegistered}>
+                  {isRegistered ? "S'inscrire" : "Deja inscrit ?"} 
+                </Button>
+              </Grid>
+
+              <Grid item xs>
+                <Button  variant="text" onClick={togglePasswordReset}>
+                  Mot de passe oublié 
+                </Button>
+                {passwordReset && <PasswordReset togglePasswordReset={togglePasswordReset}/>}
+              </Grid>
+            </Grid>
+
+          </Box>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-    
+
 export default Login
