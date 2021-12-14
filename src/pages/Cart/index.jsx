61a210f93455b77d4e177d49e266726ce955156b
Ludovic Baratier <ludovic.baratier1@gmail.com>
2021-12-14 16:59:00 +0100

    Creating cart

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -5,13 +5,59 @@ import APIManager from 'services/Api'
 import { useDispatch, useSelector } from 'react-redux'
 import userReducer from 'store/users/reducer'
 import gamesReducer from 'store/games/reducer'
-import { fetchUserRequest, fetchUserError, fetchUpdateCartSuccess } from 'store/users/actions'
-import { Container } from '@mui/material'
+import { fetchUserRequest, fetchUserError, fetchUpdateCartSuccess, fetchUpdateOrderSuccess, fetchDeleteOrderSuccess } from 'store/users/actions'
+import { Box, Container, Typography } from '@mui/material'
        
 const Cart = () => {
   const dispatch = useDispatch()
   const storedCart = useSelector(state => state.userReducer.cart)
+  const store = useSelector(state => state)
   const [cart, setCart] = useState(storedCart)
+  
+  console.log("STOOOOOORE", store)
+
+  const handleAdd = async (orderId) => {
+    const quantityElement = document.getElementById(orderId).lastChild
+    const quantity = parseInt(quantityElement.textContent.split(' ')[2])
+    const price = parseInt(quantityElement.textContent.split(' ')[0])
+
+    dispatch(fetchUserRequest())
+    const response = await APIManager.updateOrder(orderId, {quantity: quantity + 1})
+    if(response.error) {
+      dispatch(fetchUserError(response.error))
+    } else {
+      dispatch(fetchUpdateOrderSuccess(response))
+      console.log("REEEEESPONse",response)
+      quantityElement.textContent = `${price}€ x ${quantity + 1} = ${price * (quantity + 1)}€`
+    }
+  }
+
+  const handleRemove = async (orderId) => {
+    const quantityElement = document.getElementById(orderId).lastChild
+    const quantity = parseInt(quantityElement.textContent.split(' ')[2]) > 1 ? parseInt(quantityElement.textContent.split(' ')[2]) : 2
+    const price = parseInt(quantityElement.textContent.split(' ')[0])
+    
+    dispatch(fetchUserRequest())
+    const response = await APIManager.updateOrder(orderId, {quantity: quantity -1})
+    if(response.error) {
+      dispatch(fetchUserError(response.error))
+    } else {
+      dispatch(fetchUpdateOrderSuccess(response))
+      quantityElement.textContent = `${price}€ x ${quantity - 1} = ${price * (quantity - 1)}€`
+    }
+  }
+
+  const handleDelete = async (orderId) => {
+    const quantityElement = document.getElementById(orderId).parentElement
+    dispatch(fetchUserRequest())
+    const response = await APIManager.deleteOrder(orderId)
+    if(response.error) {
+      dispatch(fetchUserError(response.error))
+    } else {
+      dispatch(fetchDeleteOrderSuccess(response))
+      quantityElement.remove()
+    }
+  }
 
   useEffect (
     () => {
@@ -32,7 +78,25 @@ const Cart = () => {
 
   return (
     <Container>
-      <CartItem games={cart.cart_games} quantityButton={true} deleteButton={true}/>
+      <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+        <Typography variant="h2" color="primary" mb="0.4em" >
+          Mon panier
+        </Typography>
+        <CartItem 
+          games={cart.cart_games} 
+          quantityButton={true} 
+          deleteButton={true} 
+          handleAdd={handleAdd} 
+          handleRemove={handleRemove} 
+          handleDelete={handleDelete}
+        />
+      </Box>
     </Container>
   )
 }
