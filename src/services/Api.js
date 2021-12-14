9f1c747f5cf2df1c46540b1dad608136651b9fb0
ecko9 <jean.ferstler@gmail.com>
2021-12-14 23:25:43 +0100

    add SearchContainer and include searchbar

--- src/services/Api.js
+++ src/services/Api.js
@@ -187,11 +187,11 @@ export default class APIManager {
     // render a games array of object with and images props,
     // wich is an array of string containing the images public_id on cloudinary
     let formatedResponse = []
-      if (response.data.error){
-        formatedResponse = response.data.error
-      } else {
-        response.data.forEach( game => formatedResponse.push({...game.info, images: game.images }) )
-      }        
+    if (response.data.error) {
+      formatedResponse = response.data.error
+    } else {
+      response.data.forEach(game => formatedResponse.push({ ...game.info, images: game.images, tags: game.tags }))
+    }
     return formatedResponse
   }
 
