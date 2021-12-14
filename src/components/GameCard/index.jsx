2ffd336e79ab54f313b0986c299fb39879fef0ca
antcrn <anthonycornu.pro@gmail.com>
2021-12-14 16:30:08 +0100

    redesign cardgame list

--- src/components/GameCard/index.jsx
+++ src/components/GameCard/index.jsx
@@ -1,42 +1,34 @@
 import React from 'react'
 import { Image } from 'cloudinary-react'
 import { Card, Box, Typography, Button, Stack, Grid } from '@mui/material'
-import GameDescription from './GameDescription';
-import GameCredentials from './GameCredentials';
 import GameIconsInfos from './GameIconsInfos'
 import { Link } from 'react-router-dom';
 import APIManager from 'services/Api'
 import { useSelector } from 'react-redux';
 
 
-
 const GameCard = ({ game }) => {
   const user = useSelector(state => state.userReducer.user_info)
-  const cardHeight = window.screen.width / 8
+  const cardHeight = window.screen.width / 10
+  // window.screen.width / 3
 
   const handleRent = () => {
-    
-    const response = APIManager.createRent({quantity: 1, user_id: user.id , game_id: game.id})
-    if(!response.error) alert("jeu ajouter au favoris")
+    const response = APIManager.createRent({ quantity: 1, user_id: user.id, game_id: game.id })
+    if (!response.error) alert("jeu ajouter au favoris")
 
   }
   return (
     <>
       <Card elevation={8}
         sx={{
-          border: "1px solid",
-          borderColor: "primary.main",
-          padding: "0.3em"
+          padding: "0em",
+          borderRadius: '6px'
+
         }}
       >
-        <Link to={`/jeu/${game.id}`}>
-          <Typography variant="h4" align="center" noWrap py="0.5em" >
-            {game.name}
-          </Typography>
-        </Link>
 
         <Grid container minHeight={`${cardHeight}px`}>
-          <Grid item md={5} xs={12} display="flex" justifyContent="center" alignItems="center" overflow="hidden">
+          <Grid item lg={5} md={4} xs={12} display="flex" justifyContent="center" alignItems="center" overflow="hidden">
             <Image
               cloudName={process.env.REACT_APP_CLOUD_NAME}
               publicId={game.images ? game.images[0] : "default_game"}
@@ -52,12 +44,21 @@ const GameCard = ({ game }) => {
               height="100%"
               pr="0.2em" pl="0.8em"
             >
-              <GameDescription game={game} limit={cardHeight * 0.6} />
+              <Link to={`/jeu/${game.id}`}>
+                <Typography variant="h5" align="left" noWrap className="game-title-card">
+                  {game.name}
+                </Typography>
+              </Link>
               <GameIconsInfos game={game} />
-              <GameCredentials game={game} />
+              <Typography variant="subtitle2" align="left" noWrap color="secondary">
+                <strong className="price">
+                  {game.price}€
+                </strong>
+                <sup>    <span className="badge">{game.sell_stock > 0 && `${game.sell_stock} en stock`}</span></sup>
+              </Typography>
               <Stack direction="row" justifyContent="space-evenly">
                 <Button disabled>Acheter</Button>
-                <Button onClick={handleRent}> Louer</Button>
+                <Button onClick={handleRent} color="secondary"> Louer</Button>
               </Stack>
             </Box>
           </Grid>
