f1691c0d4470c46b29d47459a3679bd77af420e4
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-15 15:27:26 +0100

    refact subcription

--- src/components/UserSubscription/index.jsx
+++ src/components/UserSubscription/index.jsx
@@ -1,62 +1,58 @@
-import React, { useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import {Box, Tabs, Tab, Typography} from '@mui/material';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch, useStore } from 'react-redux';
-import { fetchUserRequest, fetchUserError, fetchUserSignInSuccess } from 'store/users/actions';
-import APIManager from 'services/Api';
 import Wishlist from './Wishlist';
 import CurrentRent from './CurrentRents';
-import RentHistory from './RentHistory';
+// import RentHistory from './RentHistory';
 
 
-const UserSubscription = ({user}) => {
+const UserSubscription = ({user, tiers}) => {
   const [value, setValue] = useState(0);
+
+  const wishlist = user.rent.wishlist
+  const rents = user.rent.rent_games
+  // const rentsHistory = user.rent.rented_games
+  const [userTier, setUserTier] = useState()
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-
-  const wishlist = user.wishlist
-  const rents = user.rent_games
-  const rentsHistory = user.rented_games
-
+  
   const TabPanel = () => {
     switch (value) {
       case 0:
-        return <Wishlist wishlist={wishlist} user={user}/>
+        return <Wishlist wishlist={wishlist} user={user} userTier={userTier}/>
       case 1:
         return <CurrentRent rents={rents} />
       case 2:
-        return <RentHistory rentsHistory={rentsHistory}/>
+        return ""//<RentHistory rentsHistory={rentsHistory}/>
       default:
-        return <Wishlist wishlist={wishlist}/>
+        return <Wishlist wishlist={wishlist} user={user} userTier={userTier}/>
     }
   }
 
-  const packageName = (user) => {
-    switch (user.user_info.package_id) {
-      case 1: 
-        return "Débutant"
-      case 2:
-        return "Habitué"
-      case 3:
-        return ("Confirmé")
-      default:
-        return ("")
-    }
-  }
+  React.useEffect(
+    () => {
+      if(tiers)
+        setUserTier(tiers.find(tier => tier.id === user.user_info.package_id))
+    }, [tiers, user]
+  )
 
 
   return (
     <Box display="flex" flexDirection="column" alignItems="center">
-
-      <Typography variant="h2" color="primary" align="center">Mon Abonnement</Typography>
-
-      <Typography align="center" my="1em">
-        Abonnement {packageName(user)} valide jusqu'au <Typography component="span" color="error">
-                                                         {` ${user.user_info.subscription_ending}`}.
-                                                       </Typography>
-      </Typography>
-
+      <Typography variant="h4" color="primary" align="center">Abonnement</Typography>
+      {userTier && 
+        <>
+          <Typography variant="h2" color="secondary" my={2} align="center">
+            {userTier.name}
+          </Typography>
+          <Typography align="center" my="1em">
+            valable jusqu'au <Typography component="span" color="error">
+                                  {` ${user.user_info.subscription_ending} `}
+                                </Typography>
+            et vous autorise à louer {userTier.game_number} jeu{userTier.game_number > 1 && 'x'}.
+          </Typography>
+        </>
+      }
       <Box sx={{ bgcolor: 'background.paper', maxWidth:"28em", width:"100%" }} >
         <Tabs value={value} onChange={handleChange} variant="scrollable" scrollButtons="auto">
           <Tab label="Wishlist" />
