a58867974c59a4ec7a5410650bc15ca0d5930d83
GXcode95 <81575700+GXcode95@users.noreply.github.com>
2021-12-18 14:59:40 +0100

    Update index.jsx

--- src/components/Faq/index.jsx
+++ src/components/Faq/index.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React from 'react';
 import { Container, Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 export default function Faq() {
