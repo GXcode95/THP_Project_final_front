e3f2ebdac0cb55bd823786fb88cf8cf7db14e27a
antcrn <anthonycornu.pro@gmail.com>
2021-12-18 20:04:52 +0100

    Remove Button, Stack, ghost, setGhostState from import

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,26 +1,25 @@
 import React from 'react'
-import { Box, Button, Stack, Typography} from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import './notFound.scss'
 import Dices from './Dices'
 const NotFound = () => {
-  const [ghost, setGhost] = React.useState()
 
   return (
     <div>
-      <Box 
+      <Box
         position="relative"
         display="flex"
         mt="4em"
-        sx={{justifyContent:"center"}} 
+        sx={{ justifyContent: "center" }}
       >
-        <Dices/>
+        <Dices />
       </Box>
       <Typography variant="h2" color="error" className="critical" align="center" mt="2em">
         Échec Critique !
       </Typography>
       <Typography variant="h6" align="center" className="critical-sub">
-        La page que vous avez demandé n'existe pas ... 
+        La page que vous avez demandé n'existe pas ...
       </Typography>
       <Typography variant="h4" color="secondary" align="center" className="critical-sub">
         <Link to="/jeux">Retour aux jeux</Link>
@@ -28,5 +27,5 @@ const NotFound = () => {
     </div>
   )
 }
-    
+
 export default NotFound
