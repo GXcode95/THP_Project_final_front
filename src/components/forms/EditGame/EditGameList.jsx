734971face07455f585d8506e4c2a192af438c93
antcrn <anthonycornu.pro@gmail.com>
2021-12-18 19:56:11 +0100

    Remove GameList from import

--- src/components/forms/EditGame/EditGameList.jsx
+++ src/components/forms/EditGame/EditGameList.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import GameList from 'components/GameList';
 import Games from 'pages/Games';
 
 const EditGameList = () => {
   return (
     <>
       {
-        <Games edit={true}/>
+        <Games edit={true} />
       }
     </>
   );
