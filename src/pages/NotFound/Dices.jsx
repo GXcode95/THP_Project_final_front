97d30f3a74d618d739154156a0890bf03368656c
GXcode95 <xavier.grenouillet@gmail.com>
2021-12-18 11:39:38 +0100

    add text

--- src/pages/NotFound/Dices.jsx
+++ src/pages/NotFound/Dices.jsx
@@ -2,48 +2,31 @@ import React from 'react'
 import { Button, Box} from '@mui/material'
 import './notFound.scss'
 const Dices = () => {
-  const dice = [0,1,2,3,4,5]
-
-  const rollDice = (e) => {
-    let face = ["front","back","left","right","bottom","top"]
-    
-    console.log("face",face[e.target.name])
-    let cube = document.querySelector('.cube')
-    cube.classList.remove("show-back")
-    cube.classList.remove("show-front")
-    cube.classList.remove("show-bottom")
-    cube.classList.remove("show-top")
-    cube.classList.remove("show-rigth")
-    cube.classList.remove("show-left")
-     
-    cube.classList.add(`show-${face[e.target.name]}`)
-  }
 
   return (
-      
-      <Box display="flex"  gap="5em">
-        <div className="scene">
-          <div className="cube roll-1 show-front">
-            <div className="cube__face cube__face--front">
-              <p className="colorize-1">4</p>
-            </div>
-            <div className="cube__face cube__face--back">
-              <p>1</p>
-            </div>
-            <div className="cube__face cube__face--right">
-              <p>0</p>
-            </div>
-            <div className="cube__face cube__face--left">
-              <p>5</p>
-            </div>
-            <div className="cube__face cube__face--top">
-              <p>2</p>
-            </div>
-            <div className="cube__face cube__face--bottom">
-              <p>3</p>
-            </div>
+    <Box display="flex"  gap="5em">
+      <div className="scene">
+        <div className="cube roll-1 show-front">
+          <div className="cube__face cube__face--front">
+            <p className="colorize-1">4</p>
+          </div>
+          <div className="cube__face cube__face--back">
+            <p>1</p>
+          </div>
+          <div className="cube__face cube__face--right">
+            <p>0</p>
+          </div>
+          <div className="cube__face cube__face--left">
+            <p>5</p>
+          </div>
+          <div className="cube__face cube__face--top">
+            <p>2</p>
+          </div>
+          <div className="cube__face cube__face--bottom">
+            <p>3</p>
           </div>
         </div>
+      </div>
 
       <div className="scene">
         <div className="cube roll-2 show-front">
--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
-import {  Box} from '@mui/material'
+import { Box, Button, Stack, Typography} from '@mui/material'
+import { Link } from 'react-router-dom'
 import './notFound.scss'
 import Dices from './Dices'
 const NotFound = () => {
+  const [ghost, setGhost] = React.useState()
 
   return (
-    <Box 
-      position="relative"
-      display="flex"
-      mt="4em"
-      sx={{justifyContent:"center"}} 
-    >
-      <Dices />
-    </Box>
+    <div>
+      <Box 
+        position="relative"
+        display="flex"
+        mt="4em"
+        sx={{justifyContent:"center"}} 
+      >
+        <Dices/>
+      </Box>
+      <Typography variant="h2" color="error" className="critical" align="center" mt="2em">
+        Échec Critique !
+      </Typography>
+      <Typography variant="h6" align="center" className="critical-sub">
+        La page que vous avez demandé n'existe pas ... 
+      </Typography>
+      <Typography variant="h4" color="secondary" align="center" className="critical-sub">
+        <Link to="/jeux">Retour aux jeux</Link>
+      </Typography>
+    </div>
   )
 }
     
